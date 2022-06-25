915af8c713ab1cd36fee147192e587e5cdb68809
Santiago Forero Molano <santiago.forero.molano@gmail.com>
2022-06-25 12:21:31 -0500

    feat: add task form to edit task route

--- src/routes/Task.tsx
+++ src/routes/Task.tsx
@@ -1,7 +1,9 @@
+import TaskForm from "../components/TaskForm";
+
 function Task() {
 	return (
-		<div>
-			<h1>Task</h1>
+		<div className="pt-16 pb-6 px-6 md:px-10 h-screen flex flex-col items-center">
+			<TaskForm />
 		</div>
 	);
 }
