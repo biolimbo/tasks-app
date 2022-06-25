f0560847465ac9c8610d3e08d3708086061701e4
Santiago Forero Molano <santiago.forero.molano@gmail.com>
2022-06-25 12:20:58 -0500

    fix: remove unnecessary elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,13 +6,8 @@ function Navbar() {
 	const { pathname } = useLocation();
 
 	return (
-		<nav className="bg-primary-600 text-white min-h-[60px] flex items-center px-3 md:px-6">
+		<nav className="">
 			<div className="text-xl font-medium">
-				{pathname !== "/" && (
-					<Link to="/">
-						<SVG src="/images/icons/home.svg" />
-					</Link>
-				)}
 				<span>{`Task Management > ${pathname !== "/" ? "Edit" : "Home"}`}</span>
 			</div>
 		</nav>
